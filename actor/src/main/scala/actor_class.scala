96120321f4d0683c766976447a2ef19e8d1e7501
devansh-kapoor <devansh.kapoor@knoldus.com>
2021-11-27 14:06:33 +0530

    first commit

---
+++ actor/src/main/scala/actor_class.scala
@@ -0,0 +1,5 @@
+class actor_class {
+  def main(args: Array[String]) = {
+    println("Welcome to SBT")}
+
+}
---
+++ http/src/main/scala/http_class.scala
@@ -0,0 +1,5 @@
+class http_class {
+  def main(args: Array[String]) = {
+    println("Welcome to SBT")}
+
+}
